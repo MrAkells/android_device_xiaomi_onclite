1a9f469288b7cda839654bafc825cc7d12515820
MrAkells <mrakellsreg@gmail.com>
2021-07-31 16:53:50 +0300

    onclite: AICP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/aicp_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_onclite-user \
+    aicp_onclite-userdebug \
+    aicp_onclite-eng
